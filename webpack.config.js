c19e8097d7e0308e75eb4fcd7495adfde1d04e84
Fred McDonnell <frederick.mcdonnell@outlook.com>
2019-02-10 13:08:07 +0000

    setup repository

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: 'index.jsx',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+     { test: /\.jsx$/, exclude: /node_modules/, loader: "babel-loader" },
+     { test: /\.scss$/, exclude: /node_modules/, use: ["style-loader", "css-loader"] }
+    ]
+  },
+  watchOptions: {
+    poll: 500
+  }
+};
